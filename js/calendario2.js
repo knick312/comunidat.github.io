9ed38049b34900dbf25d5e0203d86ba94a3e5d7f
knick312 <141573999+knick312@users.noreply.github.com>
2024-12-09 22:56:13 -0300

    Update calendario2.js

--- js/calendario2.js
+++ js/calendario2.js
@@ -215,7 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const eventos = [
         { fecha: new Date(anio, mes, 19), descripcion: "Streaming Especial con Nick", link: "https://twitch.tv/mundobarista" },
-       { fecha: new Date(anio, mes, 17), descripcion: "Streaming Especial con Nick", link: "https://www.twitch.tv/ciendi" },
+       { fecha: new Date(anio, mes, 17), descripcion: "hola (17/12 SHOW EN VIVO en CABA)", link: "https://www.twitch.tv/ciendi" },
     ];
 
     function renderizarCalendario() {
